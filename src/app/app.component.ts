b847cd9b13c3c712a2e678b051d100c7560c3d1d
MSK619 <msk84067@gmail.com>
2025-12-25 02:58:20 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { cssqa } from './constant/css.constant';
+import { htmlqa } from './constant/html.constant';
+import { jsqa } from './constant/js.constant';
+import { ngqa } from './constant/ng.constant';
+import { sc } from './constant/scenario.constant';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +14,40 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class AppComponent {
   selectedTopic = 'angular';
 
-  language = ['angular', 'javascript','css'];
+  language = ['angular', 'javascript','css','html','scenario'];
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  questionList:any;
 
   constructor(
   ) {}
 
-  selectTopic(topic) {
+  ngOnInit(){
+    this.questionList = ngqa;
+  }
+
+  onSelectNavItem(topic) {
     this.selectedTopic = topic;
+
+    switch(this.selectedTopic){
+      case 'angular':
+       this.questionList = ngqa;
+      break;
+      case 'javascript':
+        this.questionList = jsqa;
+      break;
+      case 'css':
+        this.questionList = cssqa;
+      break;
+      case 'html':
+          this.questionList = htmlqa;
+          break;
+      case 'scenario':
+            this.questionList = sc;
+      break;
+      default:
+        this.questionList = ngqa;
+      }
+
     this.sidenav.close();
   }
 
