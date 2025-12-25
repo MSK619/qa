4e27ec3d18a57f32172ed9346c4868d2899501cf
MSK619 <msk84067@gmail.com>
2025-12-25 11:45:50 +0530

    angular question

--- src/app/constant/ng.constant.ts
+++ src/app/constant/ng.constant.ts
@@ -26,5 +26,45 @@ export const ngqa = [
   {
     "question": "Pipes with Build-In Example.",
     "answer": ``
+  },
+  {
+    "question": "Lifecycle hooks",
+    "answer": ``
+  },
+  {
+    "question": "Directives & working of custom directive.",
+    "answer": ``
+  },
+  {
+    "question": "Design Patterns",
+    "answer": ``
+  },
+  {
+    "question": "Dependency Injection & How it works.",
+    "answer": ``
+  },
+  {
+    "question": "RXJS with some examples of operator.",
+    "answer": ``
+  },
+  {
+    "question": "Both Forms working.",
+    "answer": ``
+  },
+  {
+    "question": "NgZone",
+    "answer": ``
+  },
+  {
+    "question": "AOT & JIT Compiler",
+    "answer": ``
+  },
+  {
+    "question": "Lodash",
+    "answer": ``
+  },
+  {
+    "question": "How to updgrate Angular Project.",
+    "answer": ``
   }
 ]
