95d4210f0292810fadd3b2ba9d29ed53ef4a0485
MSK619 <msk84067@gmail.com>
2025-12-25 11:25:11 +0530

    fds

--- src/app/constant/ng.constant.ts
+++ src/app/constant/ng.constant.ts
@@ -1,6 +1,30 @@
 export const ngqa = [
   {
-    "question": "Angular 20,21 Updates?",
+    "question": "Angular 20 Updates?",
+    "answer": ``
+  },
+  {
+    "question": "Angular 21 Updates?",
+    "answer": ``
+  },
+  {
+    "question": "Signals with working.",
+    "answer": ``
+  },
+  {
+    "question": "Lazy Loading with working.",
+    "answer": ``
+  },
+  {
+    "question": "Decorators",
+    "answer": ``
+  },
+  {
+    "question": "Routing Guards",
+    "answer": ``
+  },
+  {
+    "question": "Pipes with Build-In Example.",
     "answer": ``
   }
 ]
